487465654044221aa45fe1ec323601f89e5fa448
Edgar Montes <edgar.montes@detroitmi.gov>
2019-03-29 00:29:24 -0400

    fixing filters

--- components/controller.class.js
+++ components/controller.class.js
@@ -138,26 +138,22 @@ export default class Controller {
     //console.log(ev);
     document.getElementById('initial-loader-overlay').className = 'active';
     switch (ev.target.id) {
-      case 'zipcode-filter-btn':
-        _controller.filters.zipcode = null;
-        document.getElementById('zipcode').value = '';
-        document.getElementById('zipcode-filter-btn').className = 'filter-btn';
+      case 'hardest-filter-btn':
+        _controller.filters.hardest = null;
+        document.getElementById('hardest').value = null;
+        document.getElementById('hardest-filter-btn').className = 'filter-btn';
         break;
 
-      case 'population-filter-btn':
-        _controller.filters.population = null;
-        document.getElementById('population').value = '';
-        document.getElementById('population-filter-btn').className = 'filter-btn';
+      case 'low-response-filter-btn':
+        _controller.filters.lowResponse = null;
+        document.getElementById('low-response').value = null;
+        document.getElementById('low-response-filter-btn').className = 'filter-btn';
         break;
 
-      case 'bedrooms-filter-btn':
-        _controller.filters.bedrooms = null;
-        document.getElementById('rooms').value = '';
-        document.getElementById('bedrooms-filter-btn').className = 'filter-btn';
-        break;
-
-      case 'income-filter-btn':
-        _controller.calculator.cancelIncomeFilter(_controller.calculator);
+      case 'no-internet-filter-btn':
+        _controller.filters.noInternet = null;
+        document.getElementById('no-internet').value = null;
+        document.getElementById('no-internet-filter-btn').className = 'filter-btn';
         break;
     
       default:
@@ -169,119 +165,87 @@ export default class Controller {
   updateMap(_controller){
     let filter = ['all'];
     console.log(_controller.filters);
-    if(_controller.filters.hardest == null){
-      if(_controller.filters.lowResponse == null){
-        switch (_controller.filters.noInternet) {
-          case null:
-            break;
-          
-          case '86+':
-            filter.push([">", "no_access_", 86]);
-            break;
+    switch(_controller.filters.hardest){
+      case null:
+        break;
 
-          case '60-86':
-            filter.push([">=", "no_access_", 60]);
-            filter.push(["<", "no_access_", 86]);
-            break;
+      case '73-100':
+        filter.push([">=", "mrr", 73]);
+        break;
 
-          case '40-60':
-            filter.push([">=", "no_access_", 40]);
-            filter.push(["<", "no_access_", 60]);
-            break;
+      case '70-73':
+        filter.push([">=", "mrr", 70]);
+        filter.push(["<", "mrr", 73]);
+        break;
+        
+      case '65-70':
+        filter.push([">=", "mrr", 65]);
+        filter.push(["<", "mrr", 70]);
+        break;
 
-          case '20-40':
-            filter.push([">=", "no_access_", 20]);
-            filter.push(["<", "no_access_", 40]);
-            break;
+      case '60-65':
+        filter.push([">=", "mrr", 60]);
+        filter.push(["<", "mrr", 65]);
+        break;
+
+      default:
+        filter.push([">=", "mrr", 20]);
+        filter.push(["<", "mrr", 60]);
+        break;
+    }
+    switch(_controller.filters.lowResponse){
+      case null:
+        break;
+
+      case '32.7+':
+        filter.push([">=", "low_respon", 32.7]);
+        break;
+
+      case '28.28':
+        filter.push([">=", "low_respon", 28.28]);
+        filter.push(["<", "low_respon", 32.7]);
+        break; 
+
+      case '24':
+        filter.push([">=", "low_respon", 24]);
+        filter.push(["<", "low_respon", 28.28]);
+        break; 
         
-          default:
-            filter.push([">=", "no_access_", 0]);
-            filter.push(["<", "no_access_", 20]);
-            break;
-        }
-      }else{
-        switch (_controller.filters.incomeBucket) {
-          case null:
-            where = `${_controller.filters.bedrooms}<>null`;
-            whereMaybe = '1%3D0';
-            break;
-          
-          case 'Too_High':
-            where = '1%3D0'
-            whereMaybe = '1%3D0';
-            break;
-        
-          default:
-            where = `${_controller.filters.bedrooms}<>null AND ${_controller.filters.incomeBucket}='Y'`;
-            whereMaybe = `${_controller.filters.bedrooms}<>null AND ${_controller.filters.incomeBucket}='M'`;
-            break;
-        }
-      }
-    }else{
-      if(_controller.filters.bedrooms == null){
-        switch (_controller.filters.incomeBucket) {
-          case null:
-            where = `${_controller.filters.population}<>null`;
-            whereMaybe = '1%3D0';
-            break;
-          
-          case 'Too_High':
-            where = '1%3D0';
-            whereMaybe = '1%3D0';
-            break;
-        
-          default:
-            where = `${_controller.filters.population}<>null AND ${_controller.filters.incomeBucket}='Y'`;
-            whereMaybe = `${_controller.filters.population}<>null AND ${_controller.filters.incomeBucket}='M'`;
-            break;
-        }
-      }else{
-        switch (_controller.filters.incomeBucket) {
-          case null:
-            where = `${_controller.filters.population}<>null AND ${_controller.filters.bedrooms}<>null`;
-            whereMaybe = '1%3D0';
-            break;
-          
-          case 'Too_High':
-            where = '1%3D0';
-            whereMaybe = '1%3D0';
-            break;
-        
-          default:
-            where = `${_controller.filters.population}<>null AND ${_controller.filters.bedrooms}<>null AND ${_controller.filters.incomeBucket}='Y'`;
-            whereMaybe = `${_controller.filters.population}<>null AND ${_controller.filters.bedrooms}<>null AND ${_controller.filters.incomeBucket}='M'`;
-            break;
-        }
-      }
+      default:
+        filter.push(["<", "low_respon", 24]);
+      break;
+    }
+    switch (_controller.filters.noInternet) {
+      case null:
+        break;
+      
+      case '86+':
+        filter.push([">", "no_access_", 86]);
+        break;
+
+      case '60-86':
+        filter.push([">=", "no_access_", 60]);
+        filter.push(["<", "no_access_", 86]);
+        break;
+
+      case '40-60':
+        filter.push([">=", "no_access_", 40]);
+        filter.push(["<", "no_access_", 60]);
+        break;
+
+      case '20-40':
+        filter.push([">=", "no_access_", 20]);
+        filter.push(["<", "no_access_", 40]);
+        break;
+    
+      default:
+        filter.push([">=", "no_access_", 0]);
+        filter.push(["<", "no_access_", 20]);
+        break;
     }
     _controller.map.map.setFilter('census-fill', filter);
     _controller.map.map.setFilter('census-borders', filter);
     document.getElementById('initial-loader-overlay').className = '';
-    // if(_controller.filters.zipcode != null){
-    //   let simplePolygon = turf.simplify(_controller.filters.zipcode.geometry, {tolerance: 0.005, highQuality: false});
-    //   polygon = arcGIS.convert(simplePolygon);
-    // }
-    // //console.log(where);
-    // //console.log(whereMaybe);
-    // let url = `https://services2.arcgis.com/qvkbeam7Wirps6zC/arcgis/rest/services/HRD_Website_Data(Website_View)/FeatureServer/0/query?where=${where}&objectIds=&time=&geometry=${(polygon != null) ? `${encodeURI(JSON.stringify(polygon))}`:``}&geometryType=esriGeometryPolygon&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=4326&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=geojson&token=`;
-    // //console.log(_controller.filters);
-    // //console.log(url);
-    // fetch(url)
-    // .then((resp) => resp.json()) // Transform the data into json
-    // .then(function(data) {
-    //   //console.log(data);
-    //   _controller.map.map.getSource('litch-locations').setData(data);
-
-    //   let urlMaybe = `https://services2.arcgis.com/qvkbeam7Wirps6zC/arcgis/rest/services/HRD_Website_Data(Website_View)/FeatureServer/0/query?where=${whereMaybe}&objectIds=&time=&geometry=${(polygon != null) ? `${encodeURI(JSON.stringify(polygon))}`:``}&geometryType=esriGeometryPolygon&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=4326&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=geojson&token=`;
-    //   //console.log(urlMaybe);
-    //   fetch(urlMaybe)
-    //   .then((resp) => resp.json()) // Transform the data into json
-    //   .then(function(data) {
-    //     //console.log(data);
-    //     _controller.map.map.getSource('litch-locations-maybe').setData(data);
-    //     document.getElementById('initial-loader-overlay').className = '';
-    //   });
-    // });
   }
 
   filterMap(ev, _controller){
--- components/panel.class.js
+++ components/panel.class.js
@@ -19,11 +19,40 @@ export default class Panel {
             <span class="header">Internet</span>
             <p><strong>Internet P:</strong> ${data.properties.internet_p}</p>
             <p><strong>Internet S:</strong> ${data.properties.internet_s}</p>
+            <p><strong>broadband_:</strong> ${data.properties.broadband_}</p>
+            <p><strong>no_access:</strong> ${data.properties.no_access}</p>
+            <p><strong>no_access_:</strong> ${data.properties.no_access_}</p>
+            <p><strong>no_interne:</strong> ${data.properties.no_interne}</p>
             </section>
             <section class="group">
             <span class="header">Population</span>
             <p><strong>Total Population:</strong> ${data.properties.total_pop_}</p>
             <p><strong>Total Black:</strong> ${data.properties.total_blac}</p>
+            <p><strong>black_alon:</strong> ${data.properties.black_alon}</p>
+            <p><strong>hispanic_a:</strong> ${data.properties.hispanic_a}</p>
+            <p><strong>hispanic_c:</strong> ${data.properties.hispanic_c}</p>
+            <p><strong>households:</strong> ${data.properties.households}</p>
+            <p><strong>housing_un:</strong> ${data.properties.housing_un}</p>
+            <p><strong>pop_under_:</strong> ${data.properties.pop_under_}</p>
+            <p><strong>statefp:</strong> ${data.properties.statefp}</p>
+            <p><strong>total_popu:</strong> ${data.properties.total_popu}</p>
+            </section>
+            <section class="group">
+            <span class="header">Location</span>
+            <p><strong>aland:</strong> ${data.properties.aland}</p>
+            <p><strong>awater:</strong> ${data.properties.awater}</p>
+            <p><strong>census_mai:</strong> ${data.properties.census_mai}</p>
+            <p><strong>council_di:</strong> ${data.properties.council_di}</p>
+            <p><strong>countyfp:</strong> ${data.properties.countyfp}</p>
+            <p><strong>funcstat:</strong> ${data.properties.funcstat}</p>
+            <p><strong>geoid:</strong> ${data.properties.geoid}</p>
+            <p><strong>gid:</strong> ${data.properties.gid}</p>
+            <p><strong>low_respon:</strong> ${data.properties.low_respon}</p>
+            <p><strong>mrr:</strong> ${data.properties.mrr}</p>
+            <p><strong>mtfcc:</strong> ${data.properties.mtfcc}</p>
+            <p><strong>Neighborhood:</strong> ${data.properties.neighborho}</p>
+            <p><strong>valid_mail:</strong> ${data.properties.valid_mail}</p>
+            <p><strong>Zip Codes:</strong> ${data.properties.zipcodes}</p>
             </section>
             <section class="group">
             <span class="header">Learn more</span>
@@ -31,7 +60,7 @@ export default class Panel {
                 <a class="btn resource" href="/taxonomy/term/5441" target="_blank">Get Involved</a>
             </article>
             </section>
-            <p><small>Properties may or may not have units available, and rents may vary. Property management contact information is continuously updated, as management may change. If you discover any information is not up to date, please submit a note using our <a href="https://app.smartsheet.com/b/form/1cc29c45f4694a7a97315dde550db40c" target="_blank">online form.</a></small></p>
+            <p><small>Some small print and disclaimer.</small></p>
         `;
         
         return html;
